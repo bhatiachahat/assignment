a260cbf9d0194774a8549f29adc93aa707464f3b
Chahat Bhatia <43989503+bhatiachahat@users.noreply.github.com>
2021-01-02 03:29:58 +0530

    Update operations.js

--- controller/operations.js
+++ controller/operations.js
@@ -1,13 +1,16 @@
 const fs = require('fs');
-var prompt = require('prompt-sync')();
+const prompt = require('prompt-sync')();
 const chalk = require('chalk');
 const error = chalk.bold.red;
 const success=chalk.bold.green;
 const cyan=chalk.bold.cyan;
-var HashMap = require('hashmap');
+const HashMap = require('hashmap');
 
-var fileusagemap = new HashMap();
-var keystoragemap=new HashMap();
+//map for file usage(thread safety)
+const fileusagemap = new HashMap();
+
+//map for key storage
+const keystoragemap=new HashMap();
 global.gpath="";
 const crudoperations={
      getHash(input){
@@ -33,7 +36,7 @@ const crudoperations={
       
         var Key= prompt(cyan('Enter key '));
             console.log("Key "+Key)
-            while( Key.length>32 ){ //32 chars
+            while( Key.length>32 ){ //check for 32 chars
               
                 Key= prompt(error('Key should be of less than or equal to 32 characters, Enter new valid key'));
             console.log("Key "+Key)
@@ -51,18 +54,20 @@ const crudoperations={
          const path = Path + "file" + crudoperations.getHash(Key) + ".json";
         var Value = prompt(cyan('Enter Value '));
         console.log("Value "+Value);
+         //finding size of JSON object
         const size = new TextEncoder().encode(JSON.stringify(Value)).length
       
         const kiloBytes = size / 1024;
     
     
-        while(kiloBytes > 16){  //16KB
+        while(kiloBytes > 16){  //check for less than 16KB
             if(Value=='e'){
                 return;
             }
            Value=prompt(error("Value should less than or equal to 16KB, Enter new Valid Value or press e for exit"));
          
         }
+         //time to live default will be -1 i.e. infinity
         var ttl=prompt(cyan("Enter ttl in seconds or press d for default"));
         if(ttl=='d'){
             ttl=-1;
@@ -79,32 +84,33 @@ const crudoperations={
             let newkeydata = { 
             "key" : Key,
             "value" : Value,
-            "ttl" : ttl,
-            "creationTime" : creationTime,
+             "creationTime" : creationTime,
+                  "ttl" : ttl
         };
-        console.log(newkeydata);
+     //   console.log(newkeydata);
         try{
+             // if file exists at the given path
             if (fs.existsSync(path)) {
-               
-                let rawdata = fs.readFileSync(path);
-                let filedata = JSON.parse(rawdata);
+               //Check the file size does not exceed 1GB.
+                var rawdata = fs.readFileSync(path);
+                var filedata = JSON.parse(rawdata);
          
-var totalkeys= Object.keys(filedata).length;
-var  individualkeysize = new TextEncoder().encode(JSON.stringify(filedata[0])).length
-var sizeoffile=((individualkeysize*totalkeys*1.0 )/(1024*1000000));
-console.log(sizeoffile);
-if(sizeoffile>=1){ //1GB
-console.log(error("Key can't be created.File storage is exceeding 1GB,Try deleting some keys "));
-return;
-}
+                 var totalkeys= Object.keys(filedata).length;
+                 var  individualkeysize = new TextEncoder().encode(JSON.stringify(filedata[0])).length
+                 var sizeoffile=((individualkeysize*totalkeys*1.0 )/(1024*1000000));
+                 //console.log(sizeoffile);
+                 if(sizeoffile>=1){ //Exceeding 1GB
+                 console.log(error("Key can't be created.File storage is exceeding 1GB,Try deleting some keys "));
+                  return;
+                  }
 
                
-                filedata.push(newkeydata);
-                let newFileData = JSON.stringify(filedata);
-                fs.writeFileSync(path, newFileData);
+                   filedata.push(newkeydata);
+                    var newFileData = JSON.stringify(filedata);
+                     fs.writeFileSync(path, newFileData);
             
-                keystoragemap.set(newkeydata.key, newkeydata); //key-value set in hashmap
-                console.log(success("Key Created successfully ,key "+newkeydata.key+" value "+newkeydata.value));
+                     keystoragemap.set(newkeydata.key, newkeydata); //key-value set in hashmap
+                      console.log(success("Key Created successfully ,key "+newkeydata.key+" value "+newkeydata.value));
                 // for (const pair of fileusagemap) {
                 //     console.log(`${pair.key} : ${pair.value}`)
                 // }
@@ -121,11 +127,9 @@ return;
                 
             }
             else{
-              
-                 
-                let filedata = [];
+                var filedata = [];
                 filedata.push(newkeydata);
-                let newFileData= JSON.stringify(filedata);
+                var newFileData= JSON.stringify(filedata);
                 fs.writeFileSync(path, newFileData);
                 keystoragemap.set(newkeydata.key, newkeydata);//key-value set in hashmap
            
@@ -147,10 +151,11 @@ return;
         }
         catch(error){
             console.log(error);
-            fileusagemap.set(crudoperations.getHash(Key),false);//file not in use now
+            
             
     
         }
+         fileusagemap.set(crudoperations.getHash(Key),false);//file not in use now
     
     
        
@@ -158,13 +163,14 @@ return;
     delete(){
         var Key= prompt(cyan('Enter key'));
         console.log("Key "+Key)
-        while(Key.length>32){ //32 chars
+        while(Key.length>32){ //check for 32 chars
             Key= prompt(error('Key should be of less than or equal to 32 characters, Enter new valid key or press e for exit'));
             if(Key=='e'){
                 return;
             }
             if(Key!='e') console.log("Key "+Key)
         }
+         //check if key exist
         while( keystoragemap.get(Key)==null){
             if(Key=='e'){
                 return;
@@ -175,17 +181,19 @@ return;
             }
             if(Key!='e')console.log("Key "+Key)
         }
+         try{
         fileusagemap.set(crudoperations.getHash(Key),true);//file in use now
         const path = gpath + "file" + crudoperations.getHash(Key) + ".json";
         let rawdata = fs.readFileSync(path);
         let filedata = JSON.parse(rawdata);
-
+           //check for ttl expiry, -1:infinite time, else time in seconds
         if((keystoragemap.get(Key).ttl === -1) || (((Date.now()/1000)-keystoragemap.get(Key).creationTime) < keystoragemap.get(Key).ttl)){
-           
+             //key found and is not expired
+          
             for( var i = 0; i < filedata.length; i++){ 
     
                 if ( filedata[i].key == Key) { 
-            
+                  // delete key from the file
                     filedata.splice(i, 1); 
                 }
             
@@ -198,6 +206,7 @@ return;
             console.log(success("Key deleted successfully."))
         }
         else{
+             //key found but is expired
             keystoragemap.delete(Key);
             console.log(error("Sorry,Key is expired."))
         }
@@ -205,18 +214,22 @@ return;
        
        
         fileusagemap.set(crudoperations.getHash(Key),false);//file not in use now
-
+         }
+         catch(error){
+             console.log(error);
+         }
     },
     read(){
-        var Key= prompt('Enter key');
+        var Key= prompt(cyan('Enter key'));
             console.log("Key "+Key)
-            while(Key.length>32){ //32 chars
+            while(Key.length>32){ //check for 32 chars
                 if(Key=='e'){
                     return;
                 }
                 Key= prompt(error('Key should be of less than or equal to 32 characters, Enter new valid key or press e for exit'));
                 console.log("Key "+Key)
             }
+         //key does not exist
             while( keystoragemap.get(Key)==null){
                 if(Key=='e'){
                     return;
@@ -224,16 +237,25 @@ return;
                 Key= prompt(error('Key does not exists, Enter valid key or press e for exit'));
                 console.log("Key "+Key)
             }
+         //key exists
+         try{
             fileusagemap.set(crudoperations.getHash(Key),true);//file in use now
+         //check for ttl expiry, -1:infinite time, else time in seconds
             if((keystoragemap.get(Key).ttl === -1) || (((Date.now()/1000)-keystoragemap.get(Key).creationTime) < keystoragemap.get(Key).ttl)){
+                 //key found and not expired
                 console.log("Key : "+Key+", Value : "+keystoragemap.get(Key).value)
             }
             else{
+                 //key expired
                 keystoragemap.delete(Key);
                 console.log(error("Sorry,Key is expired."))
             }
             
             fileusagemap.set(crudoperations.getHash(Key),false);//file not in use now
+         }
+         catch(error){
+          console.log(error);
+         }
     }
 }
 
